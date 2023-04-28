e55b2ac27442e95c87f6820ac69bf8e4b6392184
Mike Akiba <mike2718@gmail.com>
2023-04-29 00:45:14 +0800

    更新readme和批处理

--- make.cmd
+++ make.cmd
@@ -3,3 +3,5 @@ cd /d "%~dp0"
 
 C:\cmdtool32\iconv.exe -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.UTF8.CSV
 C:\cmdtool32\iconv.exe -f WINDOWS-936 -t UTF-8 BG7XTQ.ADI > BG7XTQ.UTF8.ADI
+
+git archive --format=zip --prefix=logbook/ --output=../logbook.zip main
