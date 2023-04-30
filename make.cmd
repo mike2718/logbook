a2b6afa73ba38e75943be4cb1ebd642d2e2fd8b9
Mike Akiba <mike2718@gmail.com>
2023-04-30 10:34:01 +0800

    由于ADIF 1不支持Unicode的字段，移除UTF-8格式的ADI文件

--- make.cmd
+++ make.cmd
@@ -2,7 +2,6 @@
 cd /d "%~dp0"
 
 C:\cmdtool32\iconv.exe -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
-C:\cmdtool32\iconv.exe -f WINDOWS-936 -t UTF-8 BG7XTQ.ADI > BG7XTQ.utf8.adi
 
-git add *.adi *.ADI *.csv *.CSV *.cbr
-git archive --format=zip --prefix=logbook/ --output=../logbook.zip main
+git add *.ADI *.csv *.CSV *.cbr *.md
+rem git archive --format=zip --prefix=logbook/ --output=../logbook.zip main
