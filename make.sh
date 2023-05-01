995153bb39f62f54c8da36772f2375eeeb3e0104
Mike Akiba <mike2718@gmail.com>
2023-05-02 04:03:33 +0800

    添加UTF-8格式的ADIF文件

--- make.sh
+++ make.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 
 iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
+iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.ADI > BG7XTQ.utf8.adi
 
 /c/cmdtool32/awk -F '","|^"|"$' -f convert.awk BG7XTQ.utf8.csv > BG7XTQ.md
 
-git add *.csv *.CSV *.ADI *.md *.awk *.sh *.cmd
+git add *.csv *.CSV *.adi *.ADI *.md *.awk *.sh *.cmd
 #git archive --format=zip --prefix=logbook/ --output=../logbook.zip main
