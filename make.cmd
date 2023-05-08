83bfca1b23c26ff8a5cff03145e99a6ff5b32e2d
Mike Akiba <mike2718@gmail.com>
2023-05-08 15:26:47 +0800

    给NOTES字段添加Markdown格式的链接，更新make.sh和make.cmd

--- make.cmd
+++ make.cmd
@@ -4,5 +4,5 @@ cd /d "%~dp0"
 C:\cmdtool32\iconv.exe -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
 C:\cmdtool32\iconv.exe -f WINDOWS-936 -t UTF-8 BG7XTQ.ADI > BG7XTQ.utf8.adi
 
-git add *.adi *.ADI *.csv *.CSV *.cbr *.md
+git add *.adi *.ADI *.csv *.CSV *.md *.sh
 rem git archive --format=zip --prefix=logbook/ --output=../logbook.zip main
