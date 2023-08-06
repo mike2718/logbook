f4ae2e39b93dd5de78815d2cbb975c5f08867656
Mike Akiba <mike2718@gmail.com>
2023-08-06 20:14:31 +0800

    更新make.sh

--- make.sh
+++ make.sh
@@ -6,4 +6,4 @@ iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.ADI > BG7XTQ.utf8.adi
 /c/cmdtool32/awk -F '","|^"|"$' -f convert.awk BG7XTQ.utf8.csv > BG7XTQ.md
 
 git add *.csv *.CSV *.adi *.ADI *.md *.awk *.sh *.cmd
-#git archive --format=zip --prefix=logbook/ --output=../logbook.zip main
+git archive --format=tar.zst --prefix=logbook/ --output=/d/logbook.tar.zst main
