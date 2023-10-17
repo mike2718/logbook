a775263fc12b19db08fda95eec0185c56ee620a6
Mike Akiba <mike2718@gmail.com>
2023-10-17 19:41:50 +0800

    更新make.sh

--- make.sh
+++ make.sh
@@ -2,7 +2,6 @@
 
 rm -f BG7XTQ.utf8.csv
 iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
-
 /c/cmdtool32/awk --csv -f convert.awk BG7XTQ.utf8.csv > BG7XTQ.md
 
 git archive --format=tar.xz --prefix=logbook/ --output=/d/logbook.tar.xz main
