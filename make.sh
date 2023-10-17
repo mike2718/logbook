0e0bfa946b8836c4136c20edb01ed5dfedd11b1d
Mike Akiba <mike2718@gmail.com>
2023-10-18 00:45:11 +0800

    修改awk脚本名

--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ git push gitlab main
 
 rm -f BG7XTQ.utf8.csv
 iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
-/c/cmdtool32/awk --csv -f convert.awk BG7XTQ.utf8.csv > BG7XTQ.md
+/c/cmdtool32/awk --csv -f csv2md.awk BG7XTQ.utf8.csv > BG7XTQ.md
 
 git add BG7XTQ.CSV BG7XTQ.ADI BG7XTQ.md BG7XTQ.utf8.csv
 
