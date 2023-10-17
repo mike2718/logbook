5b54966c2571bff5c9ed3f27967ab488b0875aca
Mike Akiba <mike2718@gmail.com>
2023-10-18 00:25:08 +0800

    添加两条QSO记录

--- make.sh
+++ make.sh
@@ -4,5 +4,7 @@ rm -f BG7XTQ.utf8.csv
 iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
 /c/cmdtool32/awk --csv -f convert.awk BG7XTQ.utf8.csv > BG7XTQ.md
 
+git add BG7XTQ.CSV BG7XTQ.ADI BG7XTQ.md BG7XTQ.utf8.csv
+
 git archive --format=tar.xz --prefix=logbook/ --output=/d/logbook.tar.xz main
 git archive --format=tar.xz --prefix=logbook/ --output=../logbook.tar.xz main
