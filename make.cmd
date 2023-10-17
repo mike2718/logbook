ffa42ede7c0b8e7f09e97d221dff722c8dc93233
Mike Akiba <mike2718@gmail.com>
2023-10-17 19:49:15 +0800

    更新make.cmd

--- make.cmd
+++ make.cmd
@@ -1,8 +1,11 @@
 @echo off
 cd /d "%~dp0"
 
+del /q BG7XTQ.utf8.csv 2>nul
 C:\cmdtool32\iconv.exe -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
-C:\cmdtool32\iconv.exe -f WINDOWS-936 -t UTF-8 BG7XTQ.ADI > BG7XTQ.utf8.adi
+C:\cmdtool32\awk.exe --csv -f convert.awk BG7XTQ.utf8.csv > BG7XTQ.md
 
-git add *.adi *.ADI *.csv *.CSV *.md *.sh
-rem git archive --format=zip --prefix=logbook/ --output=../logbook.zip main
+git archive --format=tar.xz --prefix=logbook/ --output=d:/logbook.tar.xz main
+git archive --format=tar.xz --prefix=logbook/ --output=../logbook.tar.xz main
+
+pause
