28a05d324aa5abd89894e0d9a5fde2cfa91a66bf
Mike Akiba <mike2718@gmail.com>
2023-11-06 19:34:58 +0800

    更新make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-git pull
 git push
 git push gitlab main
 
@@ -10,5 +9,5 @@ iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
 
 git add BG7XTQ.CSV BG7XTQ.ADI BG7XTQ.ASC BG7XTQ.md BG7XTQ.utf8.csv
 
-git archive --format=tar.xz --prefix=logbook/ --output=/d/logbook.tar.xz main
 git archive --format=tar.xz --prefix=logbook/ --output=../logbook.tar.xz main
+cp -f ../logbook.tar.xz /d/
