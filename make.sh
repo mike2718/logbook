2ae83777cef8330616fd3c71b411984150f29bd0
Mike Akiba <mike2718@gmail.com>
2023-11-14 13:43:44 +0800

    移除不需要的文件

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ rm -f BG7XTQ.utf8.csv
 iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
 /c/cmdtool32/awk --csv -f csv2md.awk BG7XTQ.utf8.csv > BG7XTQ.md
 
-git add BG7XTQ.CSV BG7XTQ.ADI BG7XTQ.ASC BG7XTQ.md BG7XTQ.utf8.csv
+git add BG7XTQ.CSV BG7XTQ.ADI BG7XTQ.md BG7XTQ.utf8.csv
 
 git archive --format=tar.xz --prefix=logbook/ --output=../logbook.tar.xz main
 cp -f ../logbook.tar.xz /d/
