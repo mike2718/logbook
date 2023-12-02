48d78f2ffec3f57afbb2896e6038c2c0fcd2d3ed
Mike Akiba <mike2718@gmail.com>
2023-12-02 21:48:13 +0800

    添加一条QSO记录

--- make.sh
+++ make.sh
@@ -3,11 +3,11 @@
 git push
 git push gitlab main
 
-rm -f BG7XTQ.utf8.csv
+rm -f *.utf8.csv
 iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
 /c/cmdtool32/awk --csv -f csv2md.awk BG7XTQ.utf8.csv > BG7XTQ.md
 
-git add BG7XTQ.CSV BG7XTQ.ADI BG7XTQ.md BG7XTQ.utf8.csv
+git add *.CSV *.ADI *.md *.utf8.csv
 
 git archive --format=tar.xz --prefix=logbook/ --output=../logbook.tar.xz main
 cp -f ../logbook.tar.xz /d/
