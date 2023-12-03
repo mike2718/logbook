633f037bba98335436a5773e64aa0e8cd695ce9f
Mike Akiba <mike2718@gmail.com>
2023-12-03 18:47:28 +0800

    移除不需要追踪的文件

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ rm -f *.utf8.csv
 iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
 /c/cmdtool32/awk --csv -f csv2md.awk BG7XTQ.utf8.csv > BG7XTQ.md
 
-git add *.CSV *.ADI *.md *.utf8.csv
+git add *.CSV *.ADI *.md
 
 git archive --format=tar.xz --prefix=logbook/ --output=../logbook.tar.xz main
 cp -f ../logbook.tar.xz /d/
