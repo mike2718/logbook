1208dc601c3f6119ac810d642a3baad32f3618e6
Mike Akiba <mike2718@gmail.com>
2024-01-21 15:33:13 +0800

    更新

--- make.sh
+++ make.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-git push
-git push gitlab main
-
 rm -f *.utf8.csv
 iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
 /c/cmdtool64/awk --csv -f csv2md.awk BG7XTQ.utf8.csv > BG7XTQ.md
 rm -f *.utf8.csv
 
 git add *.CSV *.ADI *.md
+git commit -m '更新'
+git push
+git push gitlab main
 
-#git archive --format=tar.xz --prefix=logbook/ --output=../logbook.tar.xz main
-#cp -f ../logbook.tar.xz /d/
+git archive --format=zip --prefix=logbook/ --output=../logbook.zip main
+cp -f ../logbook.zip /d/logbook.zip
