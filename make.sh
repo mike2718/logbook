6dd5b3a79f450610c6ccdd218124a5dcdf8e84d0
Mike Akiba <mike2718@gmail.com>
2024-02-16 03:41:25 +0800

    将markdown文件转换为crlf行结尾

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@
 rm -f *.utf8.csv
 iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
 /c/cmdtool64/awk --csv -f csv2md.awk BG7XTQ.utf8.csv > BG7XTQ.md
-rm -f *.utf8.csv
+unix2dos BG7XTQ.md
 
 git add *.CSV *.ADI *.md
 git commit -m '更新'
