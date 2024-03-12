0c2dd0e404b7b91a2c25af8654c0fb64b455e314
Mike Akiba <mike2718@gmail.com>
2024-03-12 15:48:43 +0800

    更新make.sh

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -f *.utf8.csv
+#rm -f *.utf8.csv
 #iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
 #/c/cmdtool64/awk --csv -f csv2md.awk BG7XTQ.utf8.csv > BG7XTQ.md
 /c/cmdtool32/awk --csv -f csv2md.awk BG7XTQ.CSV > BG7XTQ.md
