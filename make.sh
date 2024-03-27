013f3f59cd050aa08edc1db04409e67a3552fb45
Mike Akiba <mike2718@gmail.com>
2024-03-28 00:27:33 +0800

    修正ADIF格式日志中的结束时间数值，添加一条QSO记录

--- make.sh
+++ make.sh
@@ -4,6 +4,7 @@ rm -f BG7XTQ.utf8.csv
 iconv -f WINDOWS-936 -t UTF-8 BG7XTQ.CSV > BG7XTQ.utf8.csv
 rm -f BG7XTQ.md
 /c/cmdtool32/awk --csv -f csv2md.awk BG7XTQ.utf8.csv > BG7XTQ.md
+rm -f BG7XTQ.utf8.csv
 
 git add *.CSV *.ADI *.md
 git commit -m '更新'
